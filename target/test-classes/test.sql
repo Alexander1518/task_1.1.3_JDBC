496ffc40f1f5b69f9be16b6a4b92b1a9a814063f
Alexander <83588840+Alexander1518@users.noreply.github.com>
2021-06-06 16:09:49 +0300

    added CoreTaskTemplate

---
+++ target/test-classes/test.sql
@@ -0,0 +1,7 @@
+#insert into users (name, lastName, age) values ('Vasily', 'Vasiliev', 19);
+#insert into users (name, lastName, age) values ('Ivan', 'Ivanov', 25);
+#insert into users (name, lastName, age) values ('Alexander', 'Bezrukov', 34);
+#insert into users (name, lastName, age) values ('Sergey', 'Sergeev', 47);
+#update users set name = 'Sergey' , lastName = 'Sergeev' , age = 47;
+#select * from users;
+#delete from users;
